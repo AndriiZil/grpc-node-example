57a74e810abdfef4adb05862e6dbfaa2beba225a
Andrii Zilnyk <andrii.zilnyk@railsbank.com>
2021-04-30 19:02:28 +0300

    initial commit

---
+++ client.js
@@ -0,0 +1,26 @@
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const packageDef = protoLoader.loadSync('todo.proto', {});
+const grpcObject = grpc.loadPackageDefinition(packageDef);
+const todoPackage = grpcObject.todoPackage;
+
+const client = new todoPackage.Todo('localhost:40000', grpc.credentials.createInsecure());
+
+client.createTodo({
+  'id': -1,
+  'text': 'Marvell'
+}, (err ,response) => {
+  console.log(`Received from server ${JSON.stringify(response)}`);
+});
+
+client.readTodos({}, (err, response) => {
+  console.log(`Received from server ${JSON.stringify(response)}`);
+});
+
+// Streams
+const call = client.readTodosStream();
+call.on('data', item => {
+  console.log(`received item from the server ${JSON.stringify(item)}`);
+});
+
+call.on('end', e => console.log('server done!'));
